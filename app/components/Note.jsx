7c729f0cc255fdd389a54264e0051f24e8605a45
caraclarke <cara.clarke8@gmail.com>
2016-06-15 12:46:24 -0400

    drag working
    set up imports for new logic, defined noteSource with beginDrag handler
    
    @DragSource connects NOTe item type with noteSource
    connectDragSource prop wraps element at render

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {DragSource} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
-export default class Note extends React.Component {
+const noteSource = {
+  beginDrag(props) {
+    console.log('begin dragging notes ', props);
+
+    return {};
+  }
+};
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+
+export default class Note extends Component {
 
   render() {
-    return <li {...this.props}>{this.props.children}</li>
+    const {connectDragSource, id, onMove, ...props} = this.props
+
+    return connectDragSource (
+      <li {...props}>{props.children}</li>)
+    ;
   }
 
   renderEdit = () => {
