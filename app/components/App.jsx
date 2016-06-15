325c1643021bfa7b8c047381875e95b17ebf25ed
caraclarke <cara.clarke8@gmail.com>
2016-06-15 12:37:38 -0400

    typo fix

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,7 +5,7 @@ import LaneActions from '../actions/LaneActions';
 import LaneStore from '../stores/LaneStore';
 
 import {DragDropContext} from 'react-dnd';
-import HTML5Backend from 'react-dnd=html5-backend';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 @DragDropContext(HTML5Backend)
 
