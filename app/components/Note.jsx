c59b9a9054ac2375a17b3985b1a0fab694b27049
caraclarke <cara.clarke8@gmail.com>
2016-06-15 12:50:20 -0400

    Deleted unneeded methods, connectDropTarget
    Deleted methods that had been moved to Editable
    
    connectDropTarget gives us access to Note props using monitor.getItem to access them at noteTarget

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {DragSource} from 'react-dnd';
+import {DragSource, DropTarget} from 'react-dnd';
 import ItemTypes from '../constants/itemTypes';
 
 const noteSource = {
@@ -10,70 +10,28 @@ const noteSource = {
   }
 };
 
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+
+    console.log('dragging note ', sourceProps, targetProps);
+  }
+}
+
 @DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
   connectDragSource: connect.dragSource()
+}))
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
 }))
 
 export default class Note extends Component {
 
   render() {
-    const {connectDragSource, id, onMove, ...props} = this.props
+    const {connectDragSource, connectDropTarget, id, onMove, ...props} = this.props
 
-    return connectDragSource (
-      <li {...props}>{props.children}</li>)
-    ;
+    return connectDragSource(connectDropTarget(
+      <li {...props}>{props.children}</li>
+    ));
   }
-
-  renderEdit = () => {
-    return <input type="text"
-      ref={
-        (e) => e ? e.selectionStart = this.props.task.length : null
-      }
-      autoFocus={true}
-      defaultValue={this.props.task}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter} />;
-  };
-
-  renderDelete = () => {
-    return <button
-      className="delete-note"
-      onClick={this.props.onDelete}>x</button>;
-  };
-
-  renderNote = () => {
-    const onDelete = this.props.onDelete;
-
-    return (
-      <div onClick={this.edit}>
-        <span className="task">{this.props.task}</span>
-        {onDelete ? this.renderDelete() : null }
-      </div>
-    );
-  };
-
-  edit = () => {
-    this.setState({
-      editing: true
-    });
-  };
-
-  checkEnter = (e) => {
-    if(e.key === 'Enter') {
-      this.finishEdit(e);
-    }
-  };
-
-  finishEdit = (e) => {
-    const value = e.target.value;
-
-    if(this.props.onEdit) {
-      this.props.onEdit(value);
-
-      // Exit edit mode.
-      this.setState({
-        editing: false
-      });
-    }
-  };
 }
