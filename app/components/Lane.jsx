e808e76535b1f172b0ab963f187ef42e16576d47
caraclarke <cara.clarke8@gmail.com>
2016-06-15 11:39:43 -0400

    Spacing changes and typo fix so lane name can be edited

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,5 +1,5 @@
 import AltContainer from 'alt-container';
-import React, { Component } from 'react';
+import React from 'react';
 
 import Editable from './Editable.jsx';
 import Notes from './Notes.jsx';
@@ -7,40 +7,50 @@ import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 import LaneActions from '../actions/LaneActions';
 
-export default class Lane extends Component {
 
+export default class Lane extends React.Component {
   render() {
     const {lane, ...props} = this.props;
 
     return (
-      <div { ...props}>
-        <div className="lane-header" onCick={this.activeLaneEdit}>
-          <div className="lane-add-notes">
+      <div {...props}>
+        <div className="lane-header" onClick={this.activateLaneEdit}>
+          <div className="lane-add-note">
             <button onClick={this.addNote}>+</button>
           </div>
-            <Editable className="lane-name" editing={lane.editing} value={lane.name} onEdit={this.editName} />
-            <div className="lane-delete">
-              <button onClick={this.deleteLane}>x</button>
-            </div>
+          <Editable className="lane-name" editing={lane.editing}
+            value={lane.name} onEdit={this.editName} />
+          <div className="lane-delete">
+            <button onClick={this.deleteLane}>x</button>
+          </div>
         </div>
-          <AltContainer
-            stores={[NoteStore]}
-            inject={{
-              notes: () => NoteStore.getNotesByIds(lane.notes)
-            }}
-            >
-
-            <Notes
-              onValueClick={this.activeNoteEdit}
-              onEdit={this.editNote}
-              onDelete={this.deleteNote} />
-          </AltContainer>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getNotesByIds(lane.notes)
+          }}
+        >
+          <Notes
+            onValueClick={this.activateNoteEdit}
+            onEdit={this.editNote}
+            onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
     );
   }
 
+  editNote(id, task) {
+    // Don't modify if trying set an empty value
+    if(!task.trim()) {
+      NoteActions.update({id, editing: false});
+
+      return;
+    }
+
+    NoteActions.update({id, task, editing: false});
+  }
+
   addNote = (e) => {
-    // avoid opening lane name edit by stopping event bubbling
     e.stopPropagation();
 
     const laneId = this.props.lane.id;
@@ -50,53 +60,43 @@ export default class Lane extends Component {
       noteId: note.id,
       laneId
     });
-  }
-
-  editNote(id, task) {
-   // Don't modify if trying set an empty value
-   if(!task.trim()) {
-     NoteActions.update({id, editing: false});
-     return;
-   }
-
-   NoteActions.update({id, task, editing: false});
- }
+  };
 
   deleteNote = (noteId, e) => {
-    // avid bubbling to edit
     e.stopPropagation();
 
     const laneId = this.props.lane.id;
 
     LaneActions.detachFromLane({laneId, noteId});
     NoteActions.delete(noteId);
-  }
+  };
 
   editName = (name) => {
     const laneId = this.props.lane.id;
 
-    // dont modify if trying to set empty value
+    // Don't modify if trying set an empty value
     if(!name.trim()) {
       LaneActions.update({id: laneId, editing: false});
 
       return;
     }
-  }
+
+    LaneActions.update({id: laneId, name, editing: false});
+  };
 
   deleteLane = () => {
     const laneId = this.props.lane.id;
 
     LaneActions.delete(laneId);
-  }
+  };
 
-  activeLaneEdit = () => {
+  activateLaneEdit = () => {
     const laneId = this.props.lane.id;
 
     LaneActions.update({id: laneId, editing: true});
-  }
+  };
 
-  activeNoteEdit(id) {
+  activateNoteEdit(id) {
     NoteActions.update({id, editing: true});
   }
-
 }
