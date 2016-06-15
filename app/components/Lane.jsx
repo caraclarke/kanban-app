81f9d196143d9e8f275a9a61028702b6d05e4eec
caraclarke <cara.clarke8@gmail.com>
2016-06-15 11:29:03 -0400

    deleting lanes working, editing note name working
    set update and delete actions in LaneActions
    updated LaneStore with update and delete
    
    updated Lane component to reflect changed in actions and store

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,6 +1,7 @@
 import AltContainer from 'alt-container';
 import React, { Component } from 'react';
 
+import Editable from './Editable.jsx';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
@@ -13,11 +14,14 @@ export default class Lane extends Component {
 
     return (
       <div { ...props}>
-        <div className="lane-header">
+        <div className="lane-header" onCick={this.activeLaneEdit}>
           <div className="lane-add-notes">
             <button onClick={this.addNote}>+</button>
           </div>
-            <div className="lane-name">{lane.name}</div>
+            <Editable className="lane-name" editing={lane.editing} value={lane.name} onEdit={this.editName} />
+            <div className="lane-delete">
+              <button onClick={this.deleteLane}>x</button>
+            </div>
         </div>
           <AltContainer
             stores={[NoteStore]}
@@ -26,13 +30,19 @@ export default class Lane extends Component {
             }}
             >
 
-            <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+            <Notes
+              onValueClick={this.activeNoteEdit}
+              onEdit={this.editNote}
+              onDelete={this.deleteNote} />
           </AltContainer>
       </div>
     );
   }
 
   addNote = (e) => {
+    // avoid opening lane name edit by stopping event bubbling
+    e.stopPropagation();
+
     const laneId = this.props.lane.id;
     const note = NoteActions.create({task: 'New task'});
 
@@ -60,6 +70,33 @@ export default class Lane extends Component {
 
     LaneActions.detachFromLane({laneId, noteId});
     NoteActions.delete(noteId);
+  }
+
+  editName = (name) => {
+    const laneId = this.props.lane.id;
+
+    // dont modify if trying to set empty value
+    if(!name.trim()) {
+      LaneActions.update({id: laneId, editing: false});
+
+      return;
+    }
+  }
+
+  deleteLane = () => {
+    const laneId = this.props.lane.id;
+
+    LaneActions.delete(laneId);
+  }
+
+  activeLaneEdit = () => {
+    const laneId = this.props.lane.id;
+
+    LaneActions.update({id: laneId, editing: true});
+  }
+
+  activeNoteEdit(id) {
+    NoteActions.update({id, editing: true});
   }
 
 }
